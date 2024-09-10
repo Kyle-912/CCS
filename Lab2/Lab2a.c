9e769b528b54cf164792a9f17a08f0a05e640dce
Kyle-912 <kdt912@outlook.com>
2024-09-10 14:16:15 +0000

    Sep 10, 2024, 10:16 AM

--- Lab2/Lab2a.c
+++ Lab2/Lab2a.c
@@ -49,8 +49,8 @@ int main(void)
 
     uint8_t OPT3001_chipid = OPT3001_ReadRegister(OPT3001_DEVICEID_ADDR);
 
-    BMI160_Init(I2C_A_BASE);
-    // OPT3001_Init(OPT3001)
+    BMI160_Init();
+    OPT3001_Init();
 
     // Output chip IDs
 
