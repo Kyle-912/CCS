81530e58b832a2811c9196c086144ba45474dcf5
Kyle-912 <kdt912@outlook.com>
2024-09-10 15:45:18 +0000

    Sep 10, 2024, 11:45 AM

--- Lab2/MultimodDrivers/src/multimod_BMI160.c
+++ Lab2/MultimodDrivers/src/multimod_BMI160.c
@@ -50,8 +50,8 @@ uint8_t BMI160_ReadRegister(uint8_t addr)
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
 
     int arr;
-    I2C_ReadMultiple(I2C1_BASE, BMI160_ADDR, arr, 8);
-    
+    I2C_ReadMultiple(I2C1_BASE, BMI160_ADDR, arr, 8); //16 bit in 
+
     return;
 }
 
