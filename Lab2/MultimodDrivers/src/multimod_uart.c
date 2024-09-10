c49feedda413eaeb771b9eb187c2c131f052daf4
Kyle-912 <kdt912@outlook.com>
2024-09-10 13:55:28 +0000

    Sep 10, 2024, 9:55 AM

--- Lab2/MultimodDrivers/src/multimod_uart.c
+++ Lab2/MultimodDrivers/src/multimod_uart.c
@@ -28,6 +28,38 @@
 // Initializes UART serial communication with PC
 // Return: void
 void UART_Init() {
+    // Set system clock speed
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    // Enable relevant port for launchpad switches
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))
+    {
+    }
+
+    // Configure SW1 input
+    GPIOPinTypeGPIOInput(GPIO_PORTF_BASE, GPIO_PIN_4);
+
+    // Enable port A
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
+    {
+    }
+
+    // Enable UART0 module
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+    while (!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))
+    {
+    }
+
+    // Configure UART0 pins on port A
+    GPIOPinTypeUART(GPIO_PORTA_BASE, (GPIO_PIN_0 | GPIO_PIN_1));
+
+    // Set UART clock source
+    UARTClockSourceSet(UART0_BASE, UART_CLOCK_SYSTEM);
+
+    // Configure UART baud rate
+    UARTStdioConfig(0, 115200, SysCtlClockGet());
     // This should have been done in lab 0, so it's just copy & paste.
     // Enable port A
 
