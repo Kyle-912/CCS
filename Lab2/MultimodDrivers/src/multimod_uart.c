bcc3890adab40a255cd0cc73ef4c108c5a7fe64c
Kyle-912 <kdt912@outlook.com>
2024-09-10 14:08:23 +0000

    Sep 10, 2024, 10:08 AM

--- Lab2/MultimodDrivers/src/multimod_uart.c
+++ Lab2/MultimodDrivers/src/multimod_uart.c
@@ -28,9 +28,6 @@
 // Initializes UART serial communication with PC
 // Return: void
 void UART_Init() {
-    // Set system clock speed
-    // SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
-
     // Enable port A
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))
