1f28949e326a8a5ed7132a4f51f477cd206f9ab1
Kyle-912 <kdt912@outlook.com>
2024-09-10 15:32:40 +0000

    Sep 10, 2024, 11:32 AM

--- Lab2/MultimodDrivers/src/multimod_i2c.c
+++ Lab2/MultimodDrivers/src/multimod_i2c.c
@@ -111,8 +111,10 @@ void I2C_WriteMultiple(uint32_t mod, uint8_t addr, uint8_t *data, uint8_t num_by
     I2CMasterDataPut(mod, data);
 
     // Trigger I2C module send
+    I2CMasterControl(mod, I2C_MASTER_CMD_SINGLE_SEND);
 
     // Wait until I2C module is no longer busy
+    
 
     // While num_bytes > 1
     // Input data into I2C module
