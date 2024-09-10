5ef02f55caa241a2f4a284339a545684286369f6
Kyle-912 <kdt912@outlook.com>
2024-09-10 14:14:01 +0000

    Sep 10, 2024, 10:14 AM

--- Lab2/MultimodDrivers/src/multimod_BMI160.c
+++ Lab2/MultimodDrivers/src/multimod_BMI160.c
@@ -18,7 +18,8 @@
 // Initializes the BMI160. Currently enables the accelerometer
 // in full-power mode.
 // Return: void
-void BMI160_Init() {
+void BMI160_Init()
+{
     I2C_Init(I2C_A_BASE);
     // Power on accelerometer
 
@@ -30,7 +31,8 @@ void BMI160_Init() {
 // Param uint8_t "addr": Register address
 // Param uint8_t "data": data to write
 // Return: void
-void BMI160_WriteRegister(uint8_t addr, uint8_t data) {
+void BMI160_WriteRegister(uint8_t addr, uint8_t data)
+{
     // Complete this function
 }
 
@@ -38,7 +40,8 @@ void BMI160_WriteRegister(uint8_t addr, uint8_t data) {
 // Reads from a register address in the BMI160.
 // Param uint8_t "addr": Register address
 // Return: void
-uint8_t BMI160_ReadRegister(uint8_t addr) {
+uint8_t BMI160_ReadRegister(uint8_t addr)
+{
     // Complete this function
 }
 
@@ -48,15 +51,18 @@ uint8_t BMI160_ReadRegister(uint8_t addr) {
 // Param uint8_t* "data": pointer to an array to store data in
 // Param uint8_t "num_bytes": number of bytes to read
 // Return: void
-void BMI160_MultiReadRegister(uint8_t addr, uint8_t* data, uint8_t num_bytes) {
+void BMI160_MultiReadRegister(uint8_t addr, uint8_t *data, uint8_t num_bytes)
+{
     // Complete this function
 }
 
 // BMI160_AccelXGetResult
 // Gets the 16-bit x-axis acceleration result.
 // Return: uint16_t
-int16_t BMI160_AccelXGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC));
+int16_t BMI160_AccelXGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC))
+        ;
     uint8_t bytes[2];
 
     BMI160_MultiReadRegister(BMI160_DATA_O + ACCELX_O, bytes, 2);
@@ -67,8 +73,10 @@ int16_t BMI160_AccelXGetResult() {
 // BMI160_AccelYGetResult
 // Gets the 16-bit y-axis acceleration result.
 // Return: uint16_t
-int16_t BMI160_AccelYGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC));
+int16_t BMI160_AccelYGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC))
+        ;
 
     uint8_t bytes[2];
 
@@ -80,8 +88,10 @@ int16_t BMI160_AccelYGetResult() {
 // BMI160_AccelZGetResult
 // Gets the 16-bit z-axis acceleration result.
 // Return: uint16_t
-int16_t BMI160_AccelZGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR));
+int16_t BMI160_AccelZGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR))
+        ;
 
     uint8_t bytes[2];
 
@@ -93,8 +103,10 @@ int16_t BMI160_AccelZGetResult() {
 // BMI160_GyroXGetResult
 // Gets the 16-bit x-axis gyroscope result.
 // Return: uint16_t
-int16_t BMI160_GyroXGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR));
+int16_t BMI160_GyroXGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR))
+        ;
 
     uint8_t bytes[2];
 
@@ -106,8 +118,10 @@ int16_t BMI160_GyroXGetResult() {
 // BMI160_GyroYGetResult
 // Gets the 16-bit y-axis gyroscope result.
 // Return: uint16_t
-int16_t BMI160_GyroYGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR));
+int16_t BMI160_GyroYGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR))
+        ;
 
     uint8_t bytes[2];
 
@@ -119,8 +133,10 @@ int16_t BMI160_GyroYGetResult() {
 // BMI160_GyroZGetResult
 // Gets the 16-bit z-axis gyroscope result.
 // Return: uint16_t
-int16_t BMI160_GyroZGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR));
+int16_t BMI160_GyroZGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR))
+        ;
 
     uint8_t bytes[2];
 
@@ -132,8 +148,10 @@ int16_t BMI160_GyroZGetResult() {
 // BMI160_MagXGetResult
 // Gets the 16-bit x-axis magnetometer result.
 // Return: uint16_t
-int16_t BMI160_MagXGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG));
+int16_t BMI160_MagXGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG))
+        ;
 
     uint8_t bytes[2];
 
@@ -145,8 +163,10 @@ int16_t BMI160_MagXGetResult() {
 // BMI160_MagYGetResult
 // Gets the 16-bit y-axis magnetometer result.
 // Return: uint16_t
-int16_t BMI160_MagYGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG));
+int16_t BMI160_MagYGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG))
+        ;
 
     uint8_t bytes[2];
 
@@ -158,8 +178,10 @@ int16_t BMI160_MagYGetResult() {
 // BMI160_MagZGetResult
 // Gets the 16-bit z-axis magnetometer result.
 // Return: uint16_t
-int16_t BMI160_MagZGetResult() {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG));
+int16_t BMI160_MagZGetResult()
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG))
+        ;
 
     uint8_t bytes[2];
 
@@ -172,8 +194,10 @@ int16_t BMI160_MagZGetResult() {
 // Stores the 16-bit XYZ accelerometer results in an array.
 // Param uint16_t* "data": pointer to an array of 16-bit data.
 // Return: void
-void BMI160_AccelXYZGetResult(uint16_t* data) {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC));
+void BMI160_AccelXYZGetResult(uint16_t *data)
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_ACC))
+        ;
 
     uint8_t bytes[6];
 
@@ -190,8 +214,10 @@ void BMI160_AccelXYZGetResult(uint16_t* data) {
 // Stores the 16-bit XYZ gyroscope results in an array.
 // Param uint16_t* "data": pointer to an array of 16-bit data.
 // Return: void
-void BMI160_GyroXYZGetResult(uint16_t* data) {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR));
+void BMI160_GyroXYZGetResult(uint16_t *data)
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_GYR))
+        ;
 
     uint8_t bytes[6];
 
@@ -208,8 +234,10 @@ void BMI160_GyroXYZGetResult(uint16_t* data) {
 // Stores the 16-bit XYZ magnetometer results in an array.
 // Param uint16_t* "data": pointer to an array of 16-bit data.
 // Return: void
-void BMI160_MagXYZGetResult(uint16_t* data) {
-    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG));
+void BMI160_MagXYZGetResult(uint16_t *data)
+{
+    while (!(BMI160_GetDataStatus() & BMI160_STATUS_DRDY_MAG))
+        ;
 
     uint8_t bytes[6];
 
@@ -225,7 +253,8 @@ void BMI160_MagXYZGetResult(uint16_t* data) {
 // BMI160_GetDataStatus
 // Gets the status register to determine if data is ready to read.
 // Return: uint8_t
-uint8_t BMI160_GetDataStatus() {
+uint8_t BMI160_GetDataStatus()
+{
     return BMI160_ReadRegister(BMI160_STATUS_ADDR);
 }
 
