fcb33fb50128d3cb6f7166e961b0b6df3d142dca
Kyle-912 <kdt912@outlook.com>
2024-09-11 14:36:15 +0000

    Sep 11, 2024, 10:36 AM

--- Lab2/MultimodDrivers/src/multimod_i2c.c
+++ Lab2/MultimodDrivers/src/multimod_i2c.c
@@ -199,7 +199,7 @@ void I2C_ReadMultiple(uint32_t mod, uint8_t addr, uint8_t *data, uint8_t num_byt
     }
 
     // Read last byte
-    uint32_t data = I2CMasterDataGet(mod);
+    *data = (uint8_t)I2CMasterDataGet(mod);
 
     return;
 }
