c76ef9a55fde21db5e2a52659c42a774c7d02015
Kyle-912 <kdt912@outlook.com>
2024-09-12 13:56:17 +0000

    Sep 12, 2024, 9:56 AM

--- Lab2/MultimodDrivers/src/multimod_BMI160.c
+++ Lab2/MultimodDrivers/src/multimod_BMI160.c
@@ -46,18 +46,19 @@ uint8_t BMI160_ReadRegister(uint8_t addr)
 {
     // Complete this function
     I2C_WriteSingle(I2C1_BASE, BMI160_ADDR, addr);
-    
-    I2CMasterSlaveAddrSet(I2C1_BASE, BMI160_ADDR, false);
-    I2CMasterDataPut(I2C1_BASE, addr);
-    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
-    while (I2CMasterBusy(I2C1_BASE))
-    {
-    }
+    return I2C_ReadSingle(I2C1_BASE, BMI160_ADDR);
 
-    uint8_t arr[1];
-    I2C_ReadMultiple(I2C1_BASE, BMI160_ADDR, arr, 1);
+    // I2CMasterSlaveAddrSet(I2C1_BASE, BMI160_ADDR, false);
+    // I2CMasterDataPut(I2C1_BASE, addr);
+    // I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
+    // while (I2CMasterBusy(I2C1_BASE))
+    // {
+    // }
 
-    return arr[0];
+    // uint8_t arr[1];
+    // I2C_ReadMultiple(I2C1_BASE, BMI160_ADDR, arr, 1);
+
+    // return arr[0];
 }
 
 // BMI160_MultiReadRegister
