600f457a7ca5ba1e01d1cd0a22c1b380c104ad7f
Kyle-912 <kdt912@outlook.com>
2024-09-12 13:56:59 +0000

    Sep 12, 2024, 9:56 AM

--- Lab2/MultimodDrivers/src/multimod_i2c.c
+++ Lab2/MultimodDrivers/src/multimod_i2c.c
@@ -87,7 +87,7 @@ void I2C_WriteSingle(uint32_t mod, uint8_t addr, uint8_t byte)
 uint8_t I2C_ReadSingle(uint32_t mod, uint8_t addr)
 {
     // Set the address in the slave address register
-    I2CMasterSlaveAddrSet(mod, addr, false);
+    I2CMasterSlaveAddrSet(mod, addr, true);
 
     // Trigger I2C module receive
     I2CMasterControl(mod, I2C_MASTER_CMD_SINGLE_RECEIVE);
