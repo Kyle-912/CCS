9fa5a6029c6c06d6439c6e8b18128438fe37f208
Kyle-912 <kdt912@outlook.com>
2024-09-13 16:39:11 +0000

    Sep 13, 2024, 12:39 PM

--- Lab2/MultimodDrivers/src/multimod_i2c.c
+++ Lab2/MultimodDrivers/src/multimod_i2c.c
@@ -185,13 +185,13 @@ void I2C_ReadMultiple(uint32_t mod, uint8_t addr, uint8_t *data, uint8_t num_byt
     num_bytes--;
 
     // While num_bytes > 1
-    // Trigger I2C module receive
-    // Wait until I2C module is no longer busy
     // Read received data
     while (num_bytes > 1)
     {
+        // Trigger I2C module receive
         I2CMasterControl(mod, I2C_MASTER_CMD_BURST_RECEIVE_CONT);
 
+    // Wait until I2C module is no longer busy
         while (I2CMasterBusy(mod))
         {
         }
