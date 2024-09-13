29fd9f0a334898b0472b66bcb8e6a864903d2451
Kyle-912 <kdt912@outlook.com>
2024-09-13 16:45:59 +0000

    Sep 13, 2024, 12:45 PM

--- Lab2/Lab2a.c
+++ Lab2/Lab2a.c
@@ -53,10 +53,11 @@ int main(void)
     uint16_t OPT3001_chipid = OPT3001_ReadRegister(OPT3001_DEVICEID_ADDR);
 
     // Output chip IDs
+    UARTprintf("BMI160 Chip ID: %d\n", BMI160_chipid); // Print with label
+    UARTprintf("OPT3001 Chip ID: %d\n", OPT3001_chipid);
+
     while (1)
     {
-        UARTprintf("%d\n", BMI160_chipid);
-        UARTprintf("%d\n", OPT3001_chipid);
         SysCtlDelay(delay_0_1_s);
     }
 }
