68238fb306def93d492e958a97a42119cdab8623
Kyle-912 <kdt912@outlook.com>
2024-09-16 00:34:58 +0000

    Sep 15, 2024, 8:34 PM

---
+++ Lab2Bonus.c
@@ -0,0 +1,63 @@
+// Lab 1, uP2 Fall 2023
+// Created: 2023-07-31
+// Updated: 2023-08-01
+// Lab 1 is intended to introduce you to I2C, the sensor backpack, how to interface with them,
+// and output the values through UART.
+
+// b. Read values from sensors, UART these values
+
+/************************************Includes***************************************/
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <inc/tm4c123gh6pm.h>
+#include <inc/hw_memmap.h>
+#include <inc/hw_gpio.h>
+
+#include <driverlib/uartstdio.h>
+#include <driverlib/gpio.h>
+#include <driverlib/uart.h>
+#include <driverlib/sysctl.h>
+#include <driverlib/pin_map.h>
+#include <driverlib/interrupt.h>
+#include <driverlib/timer.h>
+
+#include "MultimodDrivers/multimod.h"
+
+/************************************Includes***************************************/
+
+/*************************************Defines***************************************/
+/*************************************Defines***************************************/
+
+/********************************Public Variables***********************************/
+/********************************Public Variables***********************************/
+
+/********************************Public Functions***********************************/
+/********************************Public Functions***********************************/
+
+/************************************MAIN*******************************************/
+
+// Read accelerometer x sensor values, output them through UART.
+int main(void)
+{
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
+    UART_Init();
+    BMI160_Init();
+    OPT3001_Init();
+
+    int16_t x_accel_value = 0;
+    uint16_t opt_value = 0;
+
+    while (1)
+    {
+        // Get accelerometer and optical sensor results, then print them to the console.
+        x_accel_value = BMI160_AccelXGetResult();
+        opt_value = OPT3001_GetResult();
+
+        UARTprintf("\rAccelerometer X: %d   Optical: %d   ", x_accel_value, opt_value);
+    }
+}
+
+/************************************MAIN*******************************************/
