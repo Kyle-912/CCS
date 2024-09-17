bccbde1cf270472686cda59f9980c3a47eb34dea
Kyle-912 <kdt912@outlook.com>
2024-09-17 15:30:19 +0000

    Sep 17, 2024, 11:30 AM

--- Lab2/Lab2c.c
+++ Lab2/Lab2c.c
@@ -63,7 +63,7 @@ void Timer_Init()
     // Disable timers
     TimerDisable(TIMER0_BASE, TIMER_A); // Timer 0A for BMI160 sampling
     TimerDisable(TIMER0_BASE, TIMER_B); // Timer 0B for OPT3001 sampling
-    TimerDisable(TIMER1_BASE, TIMER_A); // Timer 1A for
+    TimerDisable(TIMER1_BASE, TIMER_A); // Timer 1A for LED toggle
     TimerDisable(TIMER1_BASE, TIMER_B); // Timer 1B for UART output
 
     // Configure timers as half-width, periodic 16-bit or (32-bit if using 64-bit timers) timers for a total of 4 timers
