6bb467b446277726e0442461fae52c2e8f95e8b4
Kyle-912 <kdt912@outlook.com>
2024-09-23 17:02:00 +0000

    Sep 23, 2024, 1:02 PM

--- Lab3/G8RTOS/src/G8RTOS_Scheduler.c
+++ Lab3/G8RTOS/src/G8RTOS_Scheduler.c
@@ -127,7 +127,7 @@ sched_ErrCode_t G8RTOS_AddThread(void (*threadToAdd)(void))
     * 5. Point the previousTCB of the new thread to the current thread so that it moves in the correct order
     */
     if (NumberOfThreads >= MAX_THREADS)
-        return;
+        return -1;
 
     // Get the next available thread control block
     tcb_t *newTCB = &threadControlBlocks[NumberOfThreads];
