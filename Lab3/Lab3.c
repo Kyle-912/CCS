f3f1cfa1fbf0e841d1a1f3579e1d25874531ce12
Kyle-912 <kdt912@outlook.com>
2024-09-23 16:06:05 +0000

    Sep 23, 2024, 12:06 PM

--- Lab3/Lab3.c
+++ Lab3/Lab3.c
@@ -44,6 +44,8 @@ void task2()
 
 int main(void)
 {
+    SysCtlClockSet(SYSCTL_SYSDIV_1 | SYSCTL_USE_OSC | SYSCTL_OSC_MAIN | SYSCTL_XTAL_16MHZ);
+
     G8RTOS_Init();
 
     // Initializes the necessary peripherals.
