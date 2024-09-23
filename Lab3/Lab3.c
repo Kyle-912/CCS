498558848c5c5962c57c9bb3d81e51fcd513065e
Kyle-912 <kdt912@outlook.com>
2024-09-23 18:14:35 +0000

    Sep 23, 2024, 2:14 PM

--- Lab3/Lab3.c
+++ Lab3/Lab3.c
@@ -22,9 +22,9 @@
 
 /********************************Public Variables***********************************/
 
-volatile int32_t counter0 = 0;
-volatile int32_t counter1 = 0;
-volatile int32_t counter2 = 0;
+int32_t counter0 = 0;
+int32_t counter1 = 0;
+int32_t counter2 = 0;
 
 /********************************Public Variables***********************************/
 
