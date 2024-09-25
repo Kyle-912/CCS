05a168750d5eee3919f9389dcc6ef1c41bffc87a
Kyle-912 <kdt912@outlook.com>
2024-09-25 20:24:44 +0000

    Sep 25, 2024, 4:24 PM

--- Lab3/threads.c
+++ Lab3/threads.c
@@ -56,7 +56,6 @@ void Thread1(void)
         {
             absGyroX = 1.0f; // Cap at 100%
         }
-        // absGyroX *= 2.0f; // Boost brightness
 
         LaunchpadLED_PWMSetDuty(RED, absGyroX);
 
@@ -80,7 +79,6 @@ void Thread2(void)
         {
             lightLux = 1.0f; // Cap at 100%
         }
-        // lightLux *= 2.0f; // Boost brightness
 
         LaunchpadLED_PWMSetDuty(GREEN, lightLux);
 
