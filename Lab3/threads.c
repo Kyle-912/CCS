0660167e88fe8ed53bd4fde68d2a6ea7e67527af
Kyle-912 <kdt912@outlook.com>
2024-09-25 17:28:45 +0000

    Sep 25, 2024, 1:28 PM

--- Lab3/threads.c
+++ Lab3/threads.c
@@ -45,7 +45,7 @@ void Thread3(void)
 }
 
 // Thread4, reads and output button 2 status using polling
-void Thread4(void) // asdasd
+void Thread4(void)
 {
     G8RTOS_WaitSemaphore(&sem_UART);
     // Critical section: access UART
