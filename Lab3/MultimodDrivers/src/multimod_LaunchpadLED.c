a4cd0cd2ef1fbf7a544751567a9595da6adc32bd
Kyle-912 <kdt912@outlook.com>
2024-09-25 19:41:56 +0000

    Sep 25, 2024, 3:41 PM

--- Lab3/MultimodDrivers/src/multimod_LaunchpadLED.c
+++ Lab3/MultimodDrivers/src/multimod_LaunchpadLED.c
@@ -1,7 +1,7 @@
-// multimod_OPT3001.c
+// multimod_LaunchpadLED.c
 // Date Created: 2023-07-25
 // Date Updated: 2023-07-27
-// Defines for OPT3001 functions
+// Defines for LaunchpadLED functions
 
 /************************************Includes***************************************/
 
