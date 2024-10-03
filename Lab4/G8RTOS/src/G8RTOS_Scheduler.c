b8fb7c58bdbdb8498e32c830cd1fc4140d9bbcfd
Kyle-912 <kdt912@outlook.com>
2024-10-03 18:59:50 +0000

    Oct 3, 2024, 2:59 PM

--- Lab4/G8RTOS/src/G8RTOS_Scheduler.c
+++ Lab4/G8RTOS/src/G8RTOS_Scheduler.c
@@ -92,7 +92,9 @@ void SysTick_Handler()
         pt = pt->nextTCB; // Move to the next thread in the linked list
     } while (pt != CurrentlyRunningThread); // Stop when we've checked all threads
 
-    // Traverse the periodic linked list to run which functions need to be run.
+    // Traverse the periodic linked list to run which functions need to be run. TODO:
+
+    HWREG(NVIC_INT_CTRL) |= NVIC_INT_CTRL_PEND_SV;
 }
 
 // G8RTOS_Init
