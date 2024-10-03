aec9ca33a5b4cd6a2bb7dcbea36e89233d76b8bb
Kyle-912 <kdt912@outlook.com>
2024-10-03 17:51:12 +0000

    Oct 3, 2024, 1:51 PM

--- Lab4/G8RTOS/src/G8RTOS_Semaphores.c
+++ Lab4/G8RTOS/src/G8RTOS_Semaphores.c
@@ -46,7 +46,7 @@ void G8RTOS_WaitSemaphore(semaphore_t *s)
     if (*s <= 0)
     {
         CurrentlyRunningThread->blocked = s; // Set the blocked pointer
-        //TODO: yeild
+        // TODO: yield
     }
     else
     {
