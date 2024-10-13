4e0a7d722b3bd575b042b1b8e9c64ef5f3f72faf
Kyle-912 <kdt912@outlook.com>
2024-10-13 16:40:55 +0000

    Oct 13, 2024, 12:40 PM

--- Lab4/G8RTOS/src/G8RTOS_Semaphores.c
+++ Lab4/G8RTOS/src/G8RTOS_Semaphores.c
@@ -52,9 +52,9 @@ void G8RTOS_WaitSemaphore(semaphore_t *s)
         CurrentlyRunningThread->blocked = s; // Set the blocked pointer
         // EndCriticalSection(IBit_State);
         HWREG(NVIC_INT_CTRL) |= NVIC_INT_CTRL_PEND_SV; // FIXME: Yield
-        while (CurrentlyRunningThread->blocked != 0)
-        {
-        }
+        // while (CurrentlyRunningThread->blocked != 0)
+        // {
+        // }
     }
     else
     {
