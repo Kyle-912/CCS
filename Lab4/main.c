468f9c6e35a5de4017be447c984974fc11dd890d
Kyle-912 <kdt912@outlook.com>
2024-10-14 21:58:13 +0000

    Oct 14, 2024, 5:58 PM

--- Lab4/main.c
+++ Lab4/main.c
@@ -29,9 +29,7 @@ int main(void)
     multimod_init();
 
     // Add threads, semaphores, here
-    G8RTOS_InitSemaphore(&uartSemaphore, 1);
-    G8RTOS_AddThread(&SpawnerThread, 0, "Spawner");      // Spawns other self terminating threads
-    G8RTOS_Add_PeriodicEvent(&PeriodicPrinter, 1000, 0); // Add periodic event that triggers every second (1000 ticks = 1 second)
+
     G8RTOS_AddThread(&IdleThread, 255, "IdleThread");    // Idle thread with the lowest priority
 
     G8RTOS_Launch();
@@ -44,67 +42,7 @@ int main(void)
 
 /************************************Test Threads***********************************/
 
-/**
- * Thread: SpawnerThread
- * Description: Spawns self-terminating threads dynamically.
- */
-void SpawnerThread(void)
-{
-    static uint8_t threadCounter = 0;
-    while (1)
-    {
-        if (threadCounter < 5)
-        {
-            // Create a unique thread name for the new thread
-            char threadName[16] = "TermThread ";
-            threadName[11] = '0' + threadCounter; // Append thread number
-            threadName[12] = '\0';
 
-            // Dynamically add a self-terminating thread
-            G8RTOS_AddThread(&SelfTerminatingThread, 5, threadName); // Medium priority
-            threadCounter++;
-        }
-
-        sleep(5000); // Sleep for 5s before spawning another thread
-    }
-}
-
-/**
- * Thread: SelfTerminatingThread
- * Description: Runs for a while and then terminates itself.
- */
-void SelfTerminatingThread(void)
-{
-    while (1)
-    {
-        G8RTOS_WaitSemaphore(&uartSemaphore);
-        UARTprintf("Self-terminating thread started!\n");
-        G8RTOS_SignalSemaphore(&uartSemaphore);
-
-        sleep(2000); // Simulates some work by sleeping for 2s
-
-        G8RTOS_WaitSemaphore(&uartSemaphore);
-        UARTprintf("Self-terminating thread ending.\n");
-        G8RTOS_SignalSemaphore(&uartSemaphore);
-
-        G8RTOS_KillSelf();
-    }
-}
-
-/**
- * Thread: PeriodicPrinter
- * Description: Prints a message every second with an incrementing value.
- */
-void PeriodicPrinter(void)
-{
-    static uint32_t seconds = 0;
-
-    G8RTOS_WaitSemaphore(&uartSemaphore);
-    UARTprintf("Periodic event triggered, value: %d\n", seconds);
-    G8RTOS_SignalSemaphore(&uartSemaphore);
-
-    seconds++;
-}
 
 /**
  * Thread: IdleThread
