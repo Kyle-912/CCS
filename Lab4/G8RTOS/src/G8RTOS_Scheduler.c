ed130dffd67a4778b4e449d339510efbf88b997b
Kyle-912 <kdt912@outlook.com>
2024-10-14 18:35:27 +0000

    Oct 14, 2024, 2:35 PM

--- Lab4/G8RTOS/src/G8RTOS_Scheduler.c
+++ Lab4/G8RTOS/src/G8RTOS_Scheduler.c
@@ -96,7 +96,7 @@ void SysTick_Handler()
     for (int i = 0; i < NumberOfPThreads; i++)
     {
         // Check if the periodic thread's execution time has arrived
-        if (Ppt->executeTime <= SystemTime) // TODO: C
+        if (Ppt->executeTime <= SystemTime) // TODO: Consider using current time instead of 
         {
             // Run the periodic thread handler
             (*Ppt->handler)();
