2e2921fd4d91049dc7baafd677a49c497112225d
Kyle-912 <kdt912@outlook.com>
2024-10-14 18:13:41 +0000

    Oct 14, 2024, 2:13 PM

--- Lab4/G8RTOS/src/G8RTOS_Scheduler.c
+++ Lab4/G8RTOS/src/G8RTOS_Scheduler.c
@@ -329,6 +329,7 @@ sched_ErrCode_t G8RTOS_Add_PeriodicEvent(void (*PThreadToAdd)(void), uint32_t pe
     }
 
     // Check if there is no PThread. Initialize and set the first PThread.
+    ptcb_t *newPTCB = &pthreadControlBlocks[NumberOfPThreads];
 
     // Subsequent PThreads should be added, inserted similarly to a doubly-linked linked list
 
