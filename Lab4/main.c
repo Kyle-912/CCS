abc315c8f6e53bc8ad76a13450ef03d8b1f7f5f7
Kyle-912 <kdt912@outlook.com>
2024-10-14 22:02:34 +0000

    Oct 14, 2024, 6:02 PM

--- Lab4/main.c
+++ Lab4/main.c
@@ -27,7 +27,11 @@ int main(void)
 
     G8RTOS_AddThread(&IdleThread, 255, "IdleThread");    // Idle thread with the lowest priority
 
-
+    G8RTOS_InitSemaphore(&sem_I2CA, 1);              // Initialize I2C semaphore
+    G8RTOS_InitSemaphore(&sem_SPIA, 1);              // Initialize SPI semaphore
+    G8RTOS_InitSemaphore(&sem_PCA9555_Debounce, 1);  // PCA9555 button debounce semaphore
+    G8RTOS_InitSemaphore(&sem_Joystick_Debounce, 1); // Joystick press debounce semaphore
+    G8RTOS_InitSemaphore(&sem_KillCube, 1);
 
     G8RTOS_Launch();
     while (1)
