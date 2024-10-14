d2e8b244870279a6aa577804ad7ef1eadc181d32
Kyle-912 <kdt912@outlook.com>
2024-10-14 22:26:50 +0000

    Oct 14, 2024, 6:26 PM

--- Lab4/threads.c
+++ Lab4/threads.c
@@ -232,7 +232,7 @@ void Read_Buttons()
         // TODO: Process the buttons and determine what actions need to be performed.
 
         // TODO: Clear the interrupt
-
+        GPIOIntClear(GPIO_PORTE_BASE, BUTTONS_INT_PIN);
 
         // Re-enable the interrupt so it can occur again.
         G8RTOS_SignalSemaphore(&sem_PCA9555_Debounce);
