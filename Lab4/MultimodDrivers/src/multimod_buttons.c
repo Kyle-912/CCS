c3ac2c9de83e6164953f8e66e803178e7ad952f7
Kyle-912 <kdt912@outlook.com>
2024-10-21 21:02:45 +0000

    Oct 21, 2024, 5:02 PM

--- Lab4/MultimodDrivers/src/multimod_buttons.c
+++ Lab4/MultimodDrivers/src/multimod_buttons.c
@@ -24,28 +24,19 @@
 // Return: void
 void MultimodButtons_Init()
 {
-    // 1. Initialize I2C0 (PB2 for SCL, PB3 for SDA)
     I2C_Init(I2C0_BASE);
 
-    // 2. Configure PCA9555 Ports 0 and 1 as inputs
-    // Include the 0x06 register address in the I2C_WriteMultiple call
     uint8_t configData[3] = {0x06, 0xFF}; // 0x06 is the config register, set all pins as inputs
-
-    // Write the configuration data using I2C_WriteMultiple
     I2C_WriteMultiple(I2C0_BASE, PCA9555_BUTTONS_ADDR, configData, 2);
 
-    // 3. Configure the interrupt pin connected to PCA9555 INT
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE); // Enable clock to Port E
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
     {
     }
-
-    GPIOPinTypeGPIOInput(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN);              // Set as input
-    GPIOIntTypeSet(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN, GPIO_FALLING_EDGE); // Falling edge trigger
-    GPIOIntEnable(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN);                     // Enable interrupt
-    IntEnable(INT_GPIOE);                                                      // Enable Port E interrupt in NVIC
-
-    // 4. Enable global interrupts
+    GPIOPinTypeGPIOInput(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN);
+    GPIOIntTypeSet(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN, GPIO_FALLING_EDGE);
+    GPIOIntEnable(BUTTONS_INT_GPIO_BASE, BUTTONS_INT_PIN);
+    IntEnable(INT_GPIOE);
     IntMasterEnable();
 }
 
@@ -56,13 +47,10 @@ uint8_t MultimodButtons_Get()
 {
     uint8_t buttonState = 0;
 
-    // 1. Write to select Input Register 0 (Port 0) using I2C_WriteSingle
     uint8_t inputRegister = 0x00; // Command byte for Input Register 0
     I2C_WriteSingle(I2C0_BASE, PCA9555_BUTTONS_ADDR, inputRegister);
 
-    // 2. Read the input register using I2C_ReadSingle
     buttonState = I2C_ReadSingle(I2C0_BASE, PCA9555_BUTTONS_ADDR);
 
-    // 3. Mask only the relevant bits for SW1-SW4 (bits 1-4)
     return buttonState & (SW1 | SW2 | SW3 | SW4);
 }
