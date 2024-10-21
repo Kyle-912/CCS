b39e0abfed2a4ef38355504ab468aa3f5caaba64
Kyle-912 <kdt912@outlook.com>
2024-10-21 16:03:49 +0000

    Oct 21, 2024, 12:03 PM

--- Lab4/MultimodDrivers/multimod.h
+++ Lab4/MultimodDrivers/multimod.h
@@ -16,7 +16,7 @@ static void multimod_init()
     ST7789_Init();
     BMI160_Init();
     OPT3001_Init();
-    PCA9956b_Init();
+    // PCA9956b_Init();
     UART_Init();
 
     MultimodButtons_Init();
