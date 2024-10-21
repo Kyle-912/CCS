6a22c8945f2addf1b55075ba6906e1da3c69633b
Kyle-912 <kdt912@outlook.com>
2024-10-21 21:26:10 +0000

    Oct 21, 2024, 5:26 PM

--- Lab4/MultimodDrivers/src/multimod_buttons.c
+++ Lab4/MultimodDrivers/src/multimod_buttons.c
@@ -26,8 +26,8 @@ void MultimodButtons_Init()
 {
     I2C_Init(I2C0_BASE);
 
-    uint8_t configData[3] = {0x06, 0xFF, 0xFF}; // 0x06 is the config register, set all pins as inputs
-    I2C_WriteMultiple(I2C0_BASE, PCA9555_BUTTONS_ADDR, configData, 3);
+    uint8_t configData[2] = {0x06, 0xFF}; // 0x06 is the config register, set all pins as inputs
+    I2C_WriteMultiple(I2C0_BASE, PCA9555_BUTTONS_ADDR, configData, 2);
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
@@ -62,13 +62,6 @@ uint8_t MultimodButtons_Get()
 
     I2CMasterSlaveAddrSet(I2C0_BASE, PCA9555_BUTTONS_ADDR, true);    // Read mode
     I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START); // Repeated start
-    while (I2CMasterBusy(I2C0_BASE))
-    {
-    }
-
-    buttonState = I2CMasterDataGet(I2C0_BASE);
-
-    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_CONT); // Continue receiving
     while (I2CMasterBusy(I2C0_BASE))
     {
     }
