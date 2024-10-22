5c55b103639c8ada54e629a1c2b717402690a60d
Kyle-912 <kdt912@outlook.com>
2024-10-22 04:22:04 +0000

    Oct 22, 2024, 12:22 AM

--- Lab4/MultimodDrivers/src/multimod_buttons.c
+++ Lab4/MultimodDrivers/src/multimod_buttons.c
@@ -26,9 +26,6 @@ void MultimodButtons_Init()
 {
     I2C_Init(I2C1_BASE);
 
-    // uint8_t configData[2] = {0x06, 0xFF}; // 0x06 is the config register, set all pins as inputs
-    // I2C_WriteMultiple(I2C1_BASE, PCA9555_BUTTONS_ADDR, configData, 2);
-
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
     {
@@ -52,7 +49,7 @@ uint8_t MultimodButtons_Get()
 
     // buttonState = I2C_ReadSingle(I2C1_BASE, PCA9555_BUTTONS_ADDR);
 
-    /*I2CMasterSlaveAddrSet(I2C1_BASE, PCA9555_BUTTONS_ADDR, false); // Write mode
+    I2CMasterSlaveAddrSet(I2C1_BASE, PCA9555_BUTTONS_ADDR, false); // Write mode
     I2CMasterDataPut(I2C1_BASE, 0x00);                             // Select Input Register 0
 
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START); // Start (no stop)
@@ -71,16 +68,16 @@ uint8_t MultimodButtons_Get()
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH); // NACK and stop
     while (I2CMasterBusy(I2C1_BASE))
     {
-    }*/
+    }
 
-    I2CMasterSlaveAddrSet(I2C1_BASE, BUTTONS_PCA9555_GPIO_ADDR, false); // Write mode
+    /*I2CMasterSlaveAddrSet(I2C1_BASE, BUTTONS_PCA9555_GPIO_ADDR, false); // Write mode
     I2CMasterDataPut(I2C1_BASE, 0x00);                                  // Select Input Register 0
     I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START);
     while (I2CMasterBusy(I2C1_BASE))
     {
     }
 
-    buttonState = I2C_ReadSingle(I2C1_BASE, BUTTONS_PCA9555_GPIO_ADDR);
+    buttonState = I2C_ReadSingle(I2C1_BASE, BUTTONS_PCA9555_GPIO_ADDR);*/
 
     return -buttonState;
 }
