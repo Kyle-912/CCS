cb222c257f08a5266fc1a065a7b714c0ed236faa
Kyle-912 <kdt912@outlook.com>
2024-10-22 03:56:58 +0000

    Oct 21, 2024, 11:56 PM

--- Lab4/MultimodDrivers/src/multimod_buttons.c
+++ Lab4/MultimodDrivers/src/multimod_buttons.c
@@ -24,10 +24,10 @@
 // Return: void
 void MultimodButtons_Init()
 {
-    I2C_Init(I2C0_BASE);
+    I2C_Init(I2C1_BASE);
 
     // uint8_t configData[2] = {0x06, 0xFF}; // 0x06 is the config register, set all pins as inputs
-    // I2C_WriteMultiple(I2C0_BASE, PCA9555_BUTTONS_ADDR, configData, 2);
+    // I2C_WriteMultiple(I2C1_BASE, PCA9555_BUTTONS_ADDR, configData, 2);
 
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
     while (!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOE))
@@ -48,28 +48,28 @@ uint8_t MultimodButtons_Get()
     uint8_t buttonState = 0;
 
     // uint8_t inputRegister[1] = {0x00}; // Command byte for Input Register 0
-    // I2C_WriteMultiple(I2C0_BASE, PCA9555_BUTTONS_ADDR, inputRegister, 1);
+    // I2C_WriteMultiple(I2C1_BASE, PCA9555_BUTTONS_ADDR, inputRegister, 1);
 
-    // buttonState = I2C_ReadSingle(I2C0_BASE, PCA9555_BUTTONS_ADDR);
+    // buttonState = I2C_ReadSingle(I2C1_BASE, PCA9555_BUTTONS_ADDR);
 
-    I2CMasterSlaveAddrSet(I2C0_BASE, PCA9555_BUTTONS_ADDR, false); // Write mode
-    I2CMasterDataPut(I2C0_BASE, 0x00);                             // Select Input Register 0
+    I2CMasterSlaveAddrSet(I2C1_BASE, PCA9555_BUTTONS_ADDR, false); // Write mode
+    I2CMasterDataPut(I2C1_BASE, 0x00);                             // Select Input Register 0
 
-    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_SEND_START); // Start (no stop)
-    while (I2CMasterBusy(I2C0_BASE))
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_SEND_START); // Start (no stop)
+    while (I2CMasterBusy(I2C1_BASE))
     {
     }
 
-    I2CMasterSlaveAddrSet(I2C0_BASE, PCA9555_BUTTONS_ADDR, true);    // Read mode
-    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START); // Repeated start
-    while (I2CMasterBusy(I2C0_BASE))
+    I2CMasterSlaveAddrSet(I2C1_BASE, PCA9555_BUTTONS_ADDR, true);    // Read mode
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_START); // Repeated start
+    while (I2CMasterBusy(I2C1_BASE))
     {
     }
 
-    buttonState = I2CMasterDataGet(I2C0_BASE);
+    buttonState = I2CMasterDataGet(I2C1_BASE);
 
-    I2CMasterControl(I2C0_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH); // NACK and stop
-    while (I2CMasterBusy(I2C0_BASE))
+    I2CMasterControl(I2C1_BASE, I2C_MASTER_CMD_BURST_RECEIVE_FINISH); // NACK and stop
+    while (I2CMasterBusy(I2C1_BASE))
     {
     }
 
