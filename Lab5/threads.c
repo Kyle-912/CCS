c6df544864201d89499e9d0f7e51d1b0a8114905
Kyle-912 <kdt912@outlook.com>
2024-11-03 19:16:47 +0000

    Nov 3, 2024, 2:16 PM

--- Lab5/threads.c
+++ Lab5/threads.c
@@ -152,12 +152,12 @@ void Display_Thread(void)
         }
 
         // clear previous rectangle
-        ST7789_FillRect(0, previous_f1, X_MAX / 2 - 1, Y_MAX - previous_f1, ST7789_BLACK);         // FREQ1
-        ST7789_FillRect(X_MAX / 2, previous_f2, X_MAX / 2 - 1, Y_MAX - previous_f2, ST7789_BLACK); // FREQ2
+        ST7789_DrawRectangle(20, Y_MAX - previous_f1, 40, previous_f1, ST7789_BLACK); // Clear area for FREQ1
+        ST7789_DrawRectangle(60, Y_MAX - previous_f2, 40, previous_f2, ST7789_BLACK); // Clear area for FREQ2
 
         // draw new rectangle
-        ST7789_FillRect(0, Y_MAX - magnitude_f1, X_MAX / 2 - 1, magnitude_f1, ST7789_RED);          // FREQ1
-        ST7789_FillRect(X_MAX / 2, Y_MAX - magnitude_f2, X_MAX / 2 - 1, magnitude_f2, ST7789_BLUE); // FREQ2
+        ST7789_DrawRectangle(20, Y_MAX - magnitude_f1, 40, magnitude_f1, ST7789_RED);  // Draw FREQ1 bar
+        ST7789_DrawRectangle(60, Y_MAX - magnitude_f2, 40, magnitude_f2, ST7789_BLUE); // Draw FREQ2 bar
 
         G8RTOS_SignalSemaphore(&sem_SPIA);
 
