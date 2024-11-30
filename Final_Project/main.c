18d6cd130f6da671f56c0c4220681ed0b272e088
Kyle-912 <kdt912@outlook.com>
2024-11-30 21:33:51 +0000

    Nov 30, 2024, 4:33 PM

--- Final_Project/main.c
+++ Final_Project/main.c
@@ -42,9 +42,9 @@ int main(void)
     G8RTOS_InitSemaphore(&sem_Tiva_Button, 0);
 
     G8RTOS_AddThread(&Idle_Thread, 255, "IdleThread");
-    G8RTOS_AddThread(&Speaker_Thread, 0, "Speaker");
+    G8RTOS_AddThread(&Speaker_Thread, 1, "Speaker");
     G8RTOS_AddThread(&Volume_Thread, 0, "Volume");
-    G8RTOS_AddThread(&Display_Thread, 0, "Display");
+    G8RTOS_AddThread(&Display_Thread, 1, "Display");
     G8RTOS_AddThread(&JoystickPress_Thread, 0, "JoystickPress");
     G8RTOS_AddThread(&Navigation_Thread, 0, "Navigation");
     G8RTOS_AddThread(&NotePlacement_Thread, 0, "NotePlacement");
