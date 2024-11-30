598e6cde1bc38e8919d604819ab65457674897b0
Kyle-912 <kdt912@outlook.com>
2024-11-30 21:05:51 +0000

    Nov 30, 2024, 4:05 PM

--- Final_Project/main.c
+++ Final_Project/main.c
@@ -43,9 +43,9 @@ int main(void)
 
     G8RTOS_AddThread(&Idle_Thread, 255, "IdleThread");
     G8RTOS_AddThread(&Speaker_Thread, 0, "Speaker");
-    // G8RTOS_AddThread(&Volume_Thread, 0, "Volume");
+    G8RTOS_AddThread(&Volume_Thread, 0, "Volume");
     G8RTOS_AddThread(&Display_Thread, 0, "Display");
-    G8RTOS_AddThread(&JoystickPress_Thread, 0, "JoystickPress");
+    // G8RTOS_AddThread(&JoystickPress_Thread, 0, "JoystickPress");
     G8RTOS_AddThread(&Navigation_Thread, 0, "Navigation");
     G8RTOS_AddThread(&NotePlacement_Thread, 0, "NotePlacement");
 
