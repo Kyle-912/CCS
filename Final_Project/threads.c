ab435557f0fd93f045b63753d5b717bfa612a08f
Kyle-912 <kdt912@outlook.com>
2024-12-06 16:33:50 +0000

    Dec 6, 2024, 11:33 AM

--- Final_Project/threads.c
+++ Final_Project/threads.c
@@ -350,7 +350,7 @@ void JoystickPress_Thread()
         G8RTOS_WaitSemaphore(&sem_Joystick_Debounce);
 
         // Sleep to debounce
-        sleep(20);
+        sleep(15);
 
         // Switch status on the Multimod board.
         if (JOYSTICK_GetPress())
