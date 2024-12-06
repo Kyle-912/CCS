7f8408d6992fc828ae9e976dbfeaef62ec16fd35
Kyle-912 <kdt912@outlook.com>
2024-12-06 03:44:52 +0000

    Dec 5, 2024, 10:44 PM

--- Final_Project/threads.c
+++ Final_Project/threads.c
@@ -39,7 +39,7 @@ uint16_t colors[8] = {ST7789_RED, ST7789_ORANGE, ST7789_YELLOW, ST7789_GREEN, ST
 void DisplayPageNumber()
 {
     // SysCtlDelay(10000);
-    display_drawChar(5, 260, current_page, ST7789_WHITE, ST7789_WHITE, 1);
+    display_drawChar(5, 260, 1, ST7789_WHITE, ST7789_WHITE, 1);
     // display_drawChar(7, 260, '/', ST7789_WHITE, ST7789_WHITE, 1);
     // display_drawChar(20, 260, MAX_PAGES, ST7789_WHITE, ST7789_WHITE, 1);
 
